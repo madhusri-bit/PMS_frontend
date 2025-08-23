ed6ec11948ff4704b1d32a31265033d649d78c31
murugaprakash-133 <madhusri0213@gmail.com>
2025-08-23 22:35:16 +0530

    Set up done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+
+console.log("React app starting..."); 
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);

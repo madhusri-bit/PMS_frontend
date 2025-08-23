d14bae1590d153b5701500f46defa50c9a653b1c
MADHUSRI T <150882578+madhusri-bit@users.noreply.github.com>
2025-08-23 15:15:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,9 @@ const App = () => (
           <Route
             path="/"
             element={
+              <ProtectedRoute>
                 <Home />
+              </ProtectedRoute>
             }
           />
 

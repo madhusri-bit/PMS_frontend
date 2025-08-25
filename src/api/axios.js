0df293744e36d9961ccbeaa0ecf59f4df61ce9c8
MADHUSRI T <150882578+madhusri-bit@users.noreply.github.com>
2025-08-25 16:20:16 +0530

    API change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({ baseURL: 'https://pmsbackend-production-bdcd.up.railway.app/api' });
 api.interceptors.request.use((config)=>{
   const token = localStorage.getItem('token');
   if(token){ config.headers.Authorization = `Bearer ${token}`; }
